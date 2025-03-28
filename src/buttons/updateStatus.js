bb46ffcecbefd0a229886467320c6b433a0d0d7a
Samuel Liu <samuelsyliu@gmail.com>
2025-03-29 02:22:46 +1300

    replace regex per user status

--- src/buttons/updateStatus.js
+++ src/buttons/updateStatus.js
@@ -1,18 +1,23 @@
 const updateStatus = async (interaction) => {
+const content = interaction.message.content;
+const user = interaction.user.id;
+const member = interaction.guild.members.cache.get(user);
+const regex = new RegExp(String.raw`${member} Status: (.*?)\n`, "g");
+
   switch (interaction.customId) {
     case "onhold":
       await interaction.update({
-        content: "Status updated to On Hold",
+        content: content.replace(regex, `${member} Status: On Hold\n`),  
       });
       break;
     case "inprogress":
       await interaction.update({
-        content: "Status updated to In Progress",
+        content: content.replace(regex, `${member} Status: In Progress\n`),
       });
       break;
     case "done":
       await interaction.update({
-        content: "Status updated to Done",
+        content: content.replace(regex, `${member} Status: Done\n`),
       });
       break;
     default:
